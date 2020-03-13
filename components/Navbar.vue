5fd50aa79ce37842460aa3338ef52c760f27f499
Negorizont <boltuev01@mail.ru>
2020-03-13 23:27:11 +0400

    feat: create nuxt app, pages: home, about, users

---
+++ components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Nuxt SSR</a>
+
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <nuxt-link
+            exact
+            no-prefetch
+            active-class="active"
+            class="nav-link"
+            to="/"
+          >
+            <span>Home</span>
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link active-class="active" class="nav-link" to="/about">
+            <span>About</span>
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link active-class="active" class="nav-link" to="/users">
+            <span>Users</span>
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h1>About page</h1>
+
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit alias
+      voluptas, magni neque nulla omnis sint harum perspiciatis rerum ex
+      inventore ratione ipsum ea assumenda? Ratione, minima pariatur?
+      Laudantium, repellat?
+    </p>
+
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit alias
+      voluptas, magni neque nulla omnis sint harum perspiciatis rerum ex
+      inventore ratione ipsum ea assumenda?
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, officia!
+    </p>
+  </section>
+</template>
