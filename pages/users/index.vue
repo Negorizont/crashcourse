a2eaa145e75c95a0dcfd141005b22020333d9d57
Negorizont <boltuev01@mail.ru>
2020-03-14 12:33:02 +0400

    microchanges

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -3,8 +3,8 @@
     <h1>User Page</h1>
 
     <ul>
-      <li v-for="user of 5" :key="user">
-        <a class="#" @click.prevent="openUser(user)">User {{ user }}</a>
+      <li v-for="user of users" :key="user.id">
+        <a class="#" @click.prevent="openUser(user)">User {{ user.name }}</a>
       </li>
     </ul>
   </section>
@@ -12,6 +12,15 @@
 
 <script>
 export default {
+  async asyncData({ $axios }) {
+    const users = await $axios.$get(
+      'https://jsonplaceholder.typicode.com/users'
+    )
+    return { users }
+  },
+  data: () => ({
+    users: []
+  }),
   methods: {
     openUser(user) {
       this.$router.push('/users/' + user)
